7002f2014f80b083b18b8ce5d1126742063efb15
Dilawar <dilawars@ncbs.res.in>
2014-09-25 22:22:18 +0530

    We can compute the tragectories.
    Former-commit-id: 9b2320864799e8b5c564e44fb5d26bcfeafad78c
    Former-commit-id: 25a6fdc6108b60802285909c11116bf32c483831

---
+++ RandomnessInBiology_Monsoon_2014/Assignment04/solve.py
@@ -0,0 +1,47 @@
+import numpy
+import sys
+
+class Brownian():
+
+    """
+    The equation is the following 
+
+    dv = (-v/a) dt + alpha * sqrt(2*b*dt)
+
+    alpha is distributed with mean 0 and variance 1.
+    """
+
+    def __init__(self, dt, initV = 0.0, runtime = 100.0):
+        self.dt =  dt
+        self.a = 1.0 
+        self.b = 1.0
+        self.alpha = numpy.random.normal(0.0, 1.0, int(runtime/dt))
+        self.initV = initV
+        self.finalV = 0.0
+
+    def computeV(self, v, alpha):
+        dv = (- v / self.a * self.dt) + (alpha * numpy.sqrt(2.0 * self.b * self.dt))
+        return (v + dv)
+
+    def solve(self):
+        t = 0.0
+        v = self.initV
+        for a in self.alpha:
+            t = t + self.dt
+            v = self.computeV(v, a)
+        self.finalV = v
+
+def main():
+    import pylab
+    velocities = []
+    for i in range(100):
+        a = Brownian(1e-3)
+        a.solve()
+        velocities.append(a.finalV)
+    print velocities
+    pylab.hist(velocities)
+    pylab.show()
+
+
+if __name__ == '__main__':
+    main()
