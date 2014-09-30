d9a0fba587295da4bb7e1b95682d2b6507f933d8
Dilawar <dilawars@ncbs.res.in>
2014-09-30 17:40:32 +0530

    This version given the D value to be 14/100.
    Former-commit-id: bec7884a90da210d7d0b8f7682278a765afa813e
    Former-commit-id: 216169ea0823af571dd288724ec8393350f7172d

--- RandomnessInBiology_Monsoon_2014/Assignment04/solve.py
+++ RandomnessInBiology_Monsoon_2014/Assignment04/solve.py
@@ -15,6 +15,7 @@ class Brownian():
     def __init__(self, dt, initV = 0.0, runtime = 100.0):
         self.dt =  dt
         self.steps = int(runtime/dt)
+        self.dxs = numpy.zeros(self.steps)
         self.time = numpy.zeros(self.steps+1)
         self.pos = numpy.zeros(self.steps+1)
         self.a = 1.0 
@@ -22,6 +23,10 @@ class Brownian():
         self.alpha = numpy.random.normal(0.0, 1.0, self.steps)
         self.initV = initV
         self.finalV = 0.0
+        self.runtime = runtime
+
+    def reset(self):
+        self.__init__(self.dt, self.initV, self.runtime)
 
     def computeV(self, v, alpha):
         dv = (- v / self.a * self.dt) + (alpha * numpy.sqrt(2.0 * self.b * self.dt))
@@ -34,8 +39,27 @@ class Brownian():
             t = t + self.dt
             v = self.computeV(v, a)
             self.time[i+1] = t
-            self.pos[i+1] = self.pos[i] + v * self.dt
+            self.dxs[i] = v * self.dt
+            self.pos[i+1] = self.pos[i] + self.dxs[i]
         self.finalV = v
+
+    def solveMany(self, times = 1):
+        """Do the Brownian motion for n times and return the averages of values
+        """
+        dxs = []
+        finalV = []
+        finalX = []
+        for i in range(times):
+            self.reset()
+            self.solve()
+            dxs.append(self.dxs)
+            finalV.append(self.finalV)
+            finalX.append(self.pos[-1])
+        return finalX, finalV
+
+def getRMSDisplacement(pos):
+    """get root mean squared displacements of a particle """
+    return numpy.mean(numpy.square(pos))
 
 def solve_problem1():
     import pylab
@@ -47,7 +71,7 @@ def solve_problem1():
     total = 5
     for dt in timesteps:
         for v in initV:
-            print("Solving for %s timestep and %s velocty" % (dt,v))
+            print("Solving for %s timestep and %s initial velocty" % (dt,v))
             print("++ Generating %s samples" % total)
             pylab.figure()
             for i in range(total):
@@ -75,10 +99,21 @@ def solve_problem1():
         print("Problem 1 is solevd")
 
 def solve_problem2():
+    import pylab
     print("Solving problem 2")
-    for i in range(1000):
-        b = Brownian(0.001, 0)
-        b.solve()
+    rmsXList = []
+    simTimeList = []
+    displacements = []
+    for i in range(100):
+        runtime = i+1 
+        b = Brownian(0.01, 0, runtime = runtime)
+        finalX, finalV = b.solveMany(100)
+        finalX = numpy.sqrt(numpy.mean(numpy.square(finalX)))
+        displacements.append(finalX)
+    pylab.plot(displacements)
+    pylab.show()
+
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
