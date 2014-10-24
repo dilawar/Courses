c2e9711930836b5b7a15d02376343c07e4a46284
Dilawar Singh <dilawars@ncbs.res.in>
2014-10-25 00:30:22 +0530

    Added a python script to solve it.
    Former-commit-id: 7d85fc6a70d0d5af403f66a8a263bfffe4053dfb
    Former-commit-id: 81675632bd8c21da3f9291f7c62271867f9a66c8

---
+++ RandomnessInBiology_Monsoon_2014/Assignment07/solve.py
@@ -0,0 +1,68 @@
+
+"""solve.py: Soluting to homework 7.
+
+Last modified: Sat Jan 18, 2014  05:01PM
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2013, Dilawar Singh and NCBS Bangalore"
+__credits__          = ["NCBS Bangalore"]
+__license__          = "GNU GPL"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import numpy
+
+class GeneticSwitch():
+
+    def __init__(self, k1k2=1e-4, step=1e-4):
+        print("Creating genetic switch")
+        self.v0 = 12.5
+        self.v1 = 200
+        self.gamma = 1
+        self.k1k2 = k1k2
+        self.dx = 0.0
+        self.dt = 1e-1
+        self.totalSteps = int(self.dt/step)
+        self.alpha = numpy.random.normal(0, 1.0, self.totalSteps)
+        self.step = 0
+        self.time = 0.0
+        self.x = 0.0
+
+    def weinerTerm(self,  k = 1.0):
+        """A weiner term """
+        return self.alpha[self.step] * k * (self.time ** 0.5)
+
+    def dxTerm(self, x, dt=1e-6):
+        # Setup the derivative 
+        dx = ((self.v0 + (self.v1 * self.k1k2 * (x**2.0))) / (1 + self.k1k2 * (x**2.0))) - self.gamma * x
+        self.dx = dt * dx
+        return self.dx
+
+    def solveLangevian(self, dt):
+        # Solving Langevian equations.
+        output = numpy.zeros(self.totalSteps)
+        for i, e in enumerate(range(self.totalSteps)):
+            dx = self.dxTerm(self.x)
+            weiner = self.weinerTerm(k = self.x)
+            #weiner = self.weinerTerm()
+            self.x += (dx + weiner)
+            #self.x = dx
+            output[i] = self.x
+            self.time += dt
+            self.step += 1
+        return output
+
+def main():
+    import pylab
+    gs = GeneticSwitch(step=1e-5)
+    output = gs.solveLangevian(dt=1e-5)
+    pylab.plot(output)
+    pylab.show()
+
+
+if __name__ == '__main__':
+    main()
