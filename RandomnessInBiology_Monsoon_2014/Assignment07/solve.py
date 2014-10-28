b4c7708d0c4b994f197d4f29b65aa34b66f6bb28
Dilawar Singh <dilawars@ncbs.res.in>
2014-10-28 22:46:56 +0530

    With self.x as noise generating term, we get a good picture.
    Former-commit-id: a55d8f72d19a5eb2751a80481808b7864b3c4032
    Former-commit-id: 49c878d21230c6b04c053c28171ab5d8df51f688

--- RandomnessInBiology_Monsoon_2014/Assignment07/solve.py
+++ RandomnessInBiology_Monsoon_2014/Assignment07/solve.py
@@ -20,7 +20,7 @@ import sys
 class GeneticSwitch():
 
     def __init__(self, k1k2=1e-4, step=1e-4, stop=1.0, init=0.0):
-        print("Creating genetic switch")
+        print("+ Creating genetic switch")
         self.v0 = 12.5
         self.v1 = 200
         self.gamma = 1
@@ -28,33 +28,34 @@ class GeneticSwitch():
         self.dx = 0.0
         self.stop = stop
         self.step = step
-        self.totalSteps = int(self.stop/self.step)
-        self.alpha = numpy.random.normal(0, 1.0, self.totalSteps)
+        self.totalStpng = int(self.stop/self.step)
+        self.alpha = numpy.random.normal(0, 1.0, self.totalStpng)
         self.time = 0.0
         self.currentStep = 0
         self.x = init
-        self.init = init
+        self.initx = init
 
     def reinit(self):
-        self.x = self.init
+        self.x = self.initx
         self.time = 0.0
         self.currentStep = 0
         self.dx = 0.0
-        self.alpha = numpy.random.normal(0, 1.0, self.totalSteps)
+        self.alpha = numpy.random.normal(0, 1.0, self.totalStpng)
 
     def f(self, x):
         result = ((self.v0 + (self.v1 * self.k1k2 * (x**2.0))) / (1 + self.k1k2 * (x**2.0))) - self.gamma * x
         return result
 
     def g(self, x, k = 1.0):
-        result = ((self.v0 + (self.v1 * self.k1k2 * (x**2.0))) / (1 + self.k1k2 * (x**2.0))) # + self.gamma * x
+        #result = ((self.v0 + (self.v1 * self.k1k2 * (x**2.0))) / (1 + self.k1k2 * (x**2.0)))  + self.gamma * x
+        result = self.x ** 2
         return ((k*result)**0.5)
 
 
     def wienerTerm(self, x, dt = None):
         """A wiener term """
         if dt is not None:
-            result = dt * self.alpha[self.currentStep] * self.g(x)
+            result = (dt**0.5) * self.alpha[self.currentStep] * self.g(x)
         else:
             result = self.step * self.alpha[self.currentStep] * self.g(x)
         return result
@@ -76,8 +77,8 @@ class GeneticSwitch():
 
     def solveLangevian(self, withWeiner=True):
         # Solving Langevian equations.
-        output = numpy.zeros(self.totalSteps)
-        for i, e in enumerate(range(self.totalSteps)):
+        output = numpy.zeros(self.totalStpng)
+        for i, e in enumerate(range(self.totalStpng)):
             dx = self.dxTerm(self.x)
             if withWeiner:
                 wiener = self.wienerTerm(self.x)
@@ -91,12 +92,13 @@ class GeneticSwitch():
 
 def main():
     import pylab
-    gs = GeneticSwitch(k1k2=1e-4, step=1e-2, stop=200, init=0)
+    gs = GeneticSwitch(k1k2=1e-4, step=1e-1, stop=1000, init=0)
 
     # Let's calculate n trajectories of solution,
     n = 1
     collectedOutput = numpy.array([])
     cutoff = int(30.0 / gs.step)
+    cutoff = 0
     print("[INFO] cutting-off at {}".format(cutoff))
     for i in range(n):
         output = gs.solveLangevian()
@@ -111,18 +113,18 @@ def main():
     pylab.plot(x[cutoff:], output[cutoff:])
     pylab.xlabel("time in sec")
     pylab.ylabel("x (protein number)")
-    pylab.savefig('langevin_trajectories_{}.png'.format(n))
+    pylab.savefig('langevin_trajectories_{}.png'.format(gs.k1k2))
 
     #pylab.figure()
     #histOut = [ int(x) for x in collectedOutput]
     #pylab.hist(histOut)
     #pylab.show()
 
-    hist, bins = numpy.histogram(collectedOutput, bins=5)
     pylab.figure()
+    hist, bins = numpy.histogram(collectedOutput, bins=10)
     pylab.bar(bins[:-1], hist, width=1)
-    pylab.savefig('distibution_{}.png'.format(n))
-    pylab.show()
+    #pylab.hist(collectedOutput)
+    pylab.savefig('distibution_{}.png'.format(gs.k1k2))
 
 if __name__ == '__main__':
     main()
