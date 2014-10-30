b2a1aa2f3995c4403d273a19d2ce66dd2eff11ea
Dilawar Singh <dilawars@ncbs.res.in>
2014-10-31 02:19:00 +0530

    Gnuplot is not launching menu.
    Former-commit-id: e04cdc24adec6bdbc7bc5168431e66009de0faa5
    Former-commit-id: 9991a69fbeb3a39e9dea8e63c78e61e748b13a60

--- RandomnessInBiology_Monsoon_2014/Assignment07/solve.hs
+++ RandomnessInBiology_Monsoon_2014/Assignment07/solve.hs
@@ -1,6 +1,6 @@
 import Data.Random.Normal
 import System.Random
-import Graphics.EasyPlot
+import Graphics.Gnuplot.Simple
 
 alphas :: (Random a, Floating a) => IO [a]
 alphas = do
@@ -27,18 +27,28 @@ xterm x dt = do
     n <- weiner x dt 
     return $ x + (num / den - sub) * dt  + n
     where 
-        [v0, v1, k1k2, gamma] = [12.0, 200, 1e-4, 1]
+        [v0, v1, k1k2, gamma] = [12.0, 200, 1e-4, 1.0]
 
 simulate:: Float -> Float -> Int -> [Float] -> IO [Float]
-simulate x dt 0 trajectory = do
-    return trajectory 
 simulate x dt steps trajectory = do
-    x1 <- xterm x dt
-    simulate x1 dt (steps - 1) (x1:trajectory)
+    case steps of
+        0 -> do 
+            putStrLn $ "Done creating trajectory"
+            return $ reverse trajectory
+        _ -> do x1 <- xterm x dt
+                simulate x1 dt (steps-1) (x1:trajectory)
+
+simulateNTimes :: Float -> Float -> Int -> Int -> [[Float]] -> IO [[Float]]
+simulateNTimes initX dt steps n trajectories = do 
+    case n of
+        0 -> return $ reverse trajectories
+        _ -> do trajectory <- simulate initX dt steps []
+                simulateNTimes initX dt steps (n-1) (trajectory:trajectories)
 
 main = do
-    let steps = 1000
-    let dt = 1e-3
-    trajectory <- simulate (0.0::Float) dt steps []
-    let dataToPlot = zip [1,2..steps] trajectory
+    let time = 100
+    let dt = 1e-2
+    let steps = floor $ time / dt
+    trajectories <- simulateNTimes 0.0 dt steps 1 []
+    plotLists [PNG "trajectories.png"] trajectories
     putStrLn "Done"
