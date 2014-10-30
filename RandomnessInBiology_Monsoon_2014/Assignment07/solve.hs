12fb54baaf1b9462376bd3cc943ddc95e8e7b94a
Dilawar Singh <dilawars@ncbs.res.in>
2014-10-31 01:26:32 +0530

    now generate gnuplot files to print this data.
    Former-commit-id: dd6c77a1fc11621211503fad7d5fd028f91c8f5c
    Former-commit-id: 33cea7b5e80928a7d956fb9abc256002ca8117a6

---
+++ RandomnessInBiology_Monsoon_2014/Assignment07/solve.hs
@@ -0,0 +1,43 @@
+import Data.Random.Normal
+import System.Random
+import Graphics.EasyPlot
+
+alphas :: (Random a, Floating a) => IO [a]
+alphas = do
+    g <- newStdGen
+    return $ normals g
+
+-- A normal distribution. Draw a number from it.
+alpha :: (Random a, Floating a) => IO a
+alpha = normalIO 
+
+-- This is a weiner terml; evaluated at x with given dt.
+weiner :: (Floating x, Random x) => x -> x -> IO x
+weiner x dt = do 
+    a <- alpha 
+    return $ a * x * (dt ** 0.5)
+
+-- This is xterm, evaluated at x and dt
+xterm :: (Random x, Floating x) => x -> x -> IO x
+xterm x dt = do 
+    let num = (v0 + v1 * k1k2 * (x**2.0))
+    let den = (1 + k1k2 * (x**2.0))
+    let sub = gamma * x
+    n <- weiner x dt 
+    return $ x + (num / den - sub) * dt  + n
+    where 
+        [v0, v1, k1k2, gamma] = [12.0, 200, 1e-4, 1]
+
+simulate:: (Floating x, Random x, Integral b) => x -> x -> b -> [x] -> IO [x]
+simulate x dt 0 trajectory = do
+    return trajectory 
+simulate x dt steps trajectory = do
+    x1 <- xterm x dt
+    simulate x1 dt (steps - 1) (x1:trajectory)
+
+main = do
+    let steps = 1000
+    let dt = 1e-3
+    trajectory <- simulate (0.0::Float) dt steps []
+    let dataToPlot = zip [1,2..steps] trajectory
+    putStrLn "Done"
