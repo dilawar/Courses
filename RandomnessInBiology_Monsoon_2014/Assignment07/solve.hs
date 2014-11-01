f1a184c3328714afd08c5b892f6f028bac57f0dc
Dilawar Singh <dilawars@ncbs.res.in>
2014-11-01 16:01:28 +0530

    This is a pretty good working version. No need to shorted the code further. Let's write analysis functions.
    Former-commit-id: 7defd1815cc11e085ea23194c33cf39255e81629
    Former-commit-id: b887d9aa4ca7c109ab558fcd149b3d4090cb99ba

--- RandomnessInBiology_Monsoon_2014/Assignment07/solve.hs
+++ RandomnessInBiology_Monsoon_2014/Assignment07/solve.hs
@@ -4,56 +4,44 @@ import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 import Data.Foldable (toList)
 
-alphas :: (Random a, Floating a) => IO [a]
-alphas = do
-    g <- newStdGen
-    return $ normals g
-
--- A normal distribution. Draw a number from it.
---alpha :: (Random a, Floating a) => IO a
+-- A normal distribution. Draw a number from it by repeatedly calling it.
 alpha :: IO Double
 alpha = normalIO 
 
 -- This is a weiner terml; evaluated at x with given dt.
 weiner :: Double -> Double -> IO Double
 weiner x dt = do 
-    a <- alpha 
-    return $  a * ((x * dt) ** 0.5)
+    alpha >>= \a -> return $  a * ((x * dt) ** 0.5)
 
 -- This is xterm, evaluated at x and dt
 xterm :: Double -> Double -> IO Double
 xterm x dt = do 
-    let num = (v0 + v1 * k1k2 * (x**2.0))
-    let den = (1 + k1k2 * (x**2.0))
-    let sub = gamma * x
-    n <- weiner x dt 
-    return $ x + (num / den - sub) * dt  + n
-    where 
-        [v0, v1, k1k2, gamma] = [12.0, 200, 1e-4, 1.0]
+    let [v0, v1, k1k2, gamma] = [12.0, 200, 1e-4, 1.0]
+    let [num,den,sub] = [(v0 + v1 * k1k2 * (x**2.0)),(1 + k1k2 * (x**2.0)),gamma*x]
+    weiner x dt >>= \n -> return $ x + (num / den - sub) * dt  + n
 
 simulate:: Double -> Double -> Int -> [Double] -> IO [Double]
 simulate x dt steps trajectory = do
     case steps of
-        0 -> do 
-            putStrLn $ "Done creating trajectory"
-            return $ reverse trajectory
-        _ -> do x1 <- xterm x dt
-                simulate x1 dt (steps-1) (x1:trajectory)
+        0 -> return $ reverse trajectory
+        _ -> xterm x dt >>= \x1 -> simulate x1 dt (steps-1) (x1:trajectory)
 
 simulateNTimes :: Double -> Double -> Int -> Int -> [[Double]] -> IO [[Double]]
 simulateNTimes initX dt steps n trajectories = do 
     case n of
         0 -> return $ reverse trajectories
-        _ -> do trajectory <- simulate initX dt steps []
+        _ -> do 
+                trajectory <- simulate initX dt steps []
+                putStrLn $ "Created one trajectory. Left: " ++ show (n-1)
                 simulateNTimes initX dt steps (n-1) (trajectory:trajectories)
 
 main = do
-    let time = 10000
-    let dt = 1e-2
-    let steps = floor $ time / dt
-    trajectories <- simulateNTimes 0.0 dt steps 2 []
+    let (time,dt,ntimes,steps) = (10000,1e-2,2,floor $ time / dt)
+    trajectories <- simulateNTimes 0.0 dt steps ntimes []
     let dataToPlot = map (\l -> zip [1,2..steps] (toList l)) trajectories
+    putStrLn $ "Plotting trajectories"
+    -- Use cairo to plot the data.
     toFile def "trajectories.png" $ do
         layout_title .= "Trajectories"
-        mapM_ (\t -> plot (line "trajectories" [t]))  dataToPlot
-    putStrLn "Done"
+        mapM_ (\(i,t) -> plot (line ("trajectory"++show i) [t])) (zip [1,2..ntimes] dataToPlot)
+    putStrLn "Done plotting all"
