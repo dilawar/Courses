f37befbbe79e1aa539ef8d4efc8955a552dde300
Dilawar Singh <dilawars@ncbs.res.in>
2014-11-01 00:03:49 -0400

    Added global to keep global parameters.
    Former-commit-id: f3476408cd1d010cc942e43caf895012a2c305c0
    Former-commit-id: c5735ef02bab0e6b29a42d652ba059014e6533dd

--- RandomnessInBiology_Monsoon_2014/Assignment07/solve.hs
+++ RandomnessInBiology_Monsoon_2014/Assignment07/solve.hs
@@ -3,6 +3,16 @@ import System.Random
 import Graphics.Gnuplot.Simple
 import Data.Foldable (toList)
 
+-- Let's put the globals here 
+data Global = Global {
+    stepSize :: Double
+    , simTime :: Double
+    , totalSteps :: Integer
+} deriving Show 
+
+global :: Global 
+global = Global 0.01 10000.0 (floor $ simTime global/stepSize global)
+
 -- A normal distribution. Draw a number from it by repeatedly calling it.
 alpha :: IO Double
 alpha = normalIO 
@@ -19,25 +29,22 @@ xterm x dt = do
     let [num,den,sub] = [(v0 + v1 * k1k2 * (x**2.0)),(1 + k1k2 * (x**2.0)),gamma*x]
     weiner x dt >>= \n -> return $ x + (num / den - sub) * dt  + n
 
-simulate:: Double -> Double -> Int -> [Double] -> IO [Double]
-simulate x dt steps trajectory = do
+simulate:: Double -> Integer -> [Double] -> IO [Double]
+simulate x steps trajectory = do
+    let dt = stepSize global
     case steps of
         0 -> return $ reverse trajectory
-        _ -> xterm x dt >>= \x1 -> simulate x1 dt (steps-1) (x1:trajectory)
+        _ -> xterm x dt >>= \x1 -> simulate x1 (steps-1) (x1:trajectory)
 
-simulateNTimes :: Double -> Double -> Int -> Int -> [[Double]] -> IO [[Double]]
-simulateNTimes initX dt steps n trajectories = do 
+simulateNTimes :: Double  -> Int -> [[Double]] -> IO [[Double]]
+simulateNTimes initX n trajectories = do 
+    let steps = totalSteps global
     case n of
         0 -> return $ reverse trajectories
         _ -> do 
-                trajectory <- simulate initX dt steps []
+                trajectory <- simulate initX steps []
                 putStrLn $ "Created one trajectory. Left: " ++ show (n-1)
-                simulateNTimes initX dt steps (n-1) (trajectory:trajectories)
-
+                simulateNTimes initX (n-1) (trajectory:trajectories)
 main = do
     let (time,dt,ntimes,steps) = (10000,1e-2, 3, floor $ time / dt)
-    trajectories <- simulateNTimes 0.0 dt steps ntimes []
-    let dataToPlot = map (\l -> zip [1,2..steps] (toList l)) trajectories
-    putStrLn $ "Plotting trajectories"
-    plotLists [PNG "trajectories.png"] dataToPlot
-    putStrLn "Done plotting all"
+    trajectories <- simulateNTimes 0.0 ntimes []
