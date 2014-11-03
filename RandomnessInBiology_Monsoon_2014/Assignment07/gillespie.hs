0555f029fd41541b0596c06b578795ccda75d6fa
Dilawar Singh <dilawars@ncbs.res.in>
2014-11-03 10:54:03 +0530

    Added gillespie. Need more work.
    Former-commit-id: 720874b2cc62e04ec077a7653f960ea3051d04ac
    Former-commit-id: 37d6bd51edaf849a795ac76982ca73c035dd2ee9

--- RandomnessInBiology_Monsoon_2014/Assignment07/gillespie.hs
+++ RandomnessInBiology_Monsoon_2014/Assignment07/gillespie.hs
@@ -8,26 +8,29 @@ import System.Random
 
 -- Global parameters.
 data Global = Global {
-    birthRate :: Double
-    , deathRate :: Double
-    , threshold :: Double
+    threshold :: Double
     } deriving Show 
 
 data Event = Birth | Death deriving (Show, Eq)
 
-global = Global 0.04 0.04 0.4
+global = Global 0.4
+fI = fromIntegral
 
 randomNums :: Int -> [Double]
 randomNums n = randoms  (mkStdGen n)
 
+birthRate :: Int -> Double
+birthRate x = (12.0 + 200*1e-4*(fI x^2))/(1 + 200*(fI x^2)) * (1 / fI x)
+deathRate :: Int -> Double
+deathRate x = 2/fI x
 -- This function determines which events should occur
 birthOrDeath :: Double -> Event
 birthOrDeath r | r < (threshold global) = Death
                | otherwise = Birth 
 
 event :: Int -> Event -> Int
-event n Birth = n + (floor $ (birthRate global) * fromIntegral n)
-event n Death = n - (floor $ (deathRate global) * fromIntegral n)
+event n Birth = n + (floor $ (birthRate n) * fI n)
+event n Death = n - (floor $ (deathRate n) * fI n)
 
 reaction :: [Int] -> [Double] -> [Int]
 reaction n [] = reverse n
@@ -38,5 +41,5 @@ reaction n rands
           e = birthOrDeath (head rands)
 
 -- Run the equation with seed n. n controls the random numbers generated.
-run n steps = reaction [100] (take steps $ randomNums n)
+run seed initX steps = reaction [initX] (take steps $ randomNums seed)
 
