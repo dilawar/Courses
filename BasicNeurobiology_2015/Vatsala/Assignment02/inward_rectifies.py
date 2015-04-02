2687b3fc139c8edc6d03a5ec09b00e114ef5597e
Dilawar Singh <dilawars@ncbs.res.in>
2015-04-02 21:13:20 +0530

    Python script to generate curve.
    Former-commit-id: 809a08d0b7eb6f0336f12e70c5077fc438699e0d
    Former-commit-id: f245658640e500073f736197eb2ba33a54a2fd0c

---
+++ BasicNeurobiology_2015/Vatsala/Assignment02/inward_rectifies.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pylab
+
+def channel(vm):
+    time = np.arange(0, 150e-3, 1e-3)
+    tau1, tau2, tau3 = 1e-3, 10e-3, 10000e-3
+    c = -vm/10e-3
+    c = (abs(c) + c)/2
+    print c
+    c1 = c**0.2
+    current = np.zeros(len(time))
+    for i, t in enumerate(time):
+        current[i] = c * np.exp(-t/tau1) - c1*np.exp(-t/tau2) - c*np.exp(-t/tau3)
+        current[i] += np.random.random()/50
+    pylab.plot(time, current, label="%s V"%vm)
+    pylab.legend()
+    return  time, current
+
+
+def main():
+    channel(-80e-3)
+    channel(-30e-3)
+    channel(0e-3)
+    channel(30e-3)
+    pylab.title("Invard rectifying K-current")
+    pylab.savefig("inward_rectifying.png")
+
+
+if __name__ == '__main__':
+    main()
