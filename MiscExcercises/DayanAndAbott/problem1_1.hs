740dfae7f9fbf90f2027b0abdc9a64d28ee3b7fb
Dilawar Singh <dilawars@ncbs.res.in>
2015-05-10 20:44:20 +0530

    Added.
    Former-commit-id: 6ecf6ee5c4edf91dff600237ec375fbe921717e0
    Former-commit-id: 346809e031ea0d35bbb9a256b7abadd0dc0da62d

--- MiscExcercises/DayanAndAbott/problem1_1.hs
+++ MiscExcercises/DayanAndAbott/problem1_1.hs
@@ -1,7 +1,16 @@
 -- Chapter 1, Problem 1
 
 import GSL.Random.Dist
+import GSL.Random.Gen
+import System.Random
 
+-- Following uses GSL
 rate = 100
-t = 1
-dist = map (\x -> poissonPdf x 1.0) [1..(rate*t)]
+time = 10
+rng = newRNG mt19937
+next =  rng >>= \x -> getFlat x 0.0 1.0 
+    
+samples = time * rate
+main = do
+    g <- getStdGen
+    print $ take samples (randoms g :: [Double])
