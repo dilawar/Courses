479ce9c76ce2964a7d493ff60c5ff28c28caccdc
Dilawar Singh <dilawars@ncbs.res.in>
2015-09-27 17:30:40 +0530

    Solved problem 1.
    Former-commit-id: fb25ef752975b3cc88a7d6843bd46da9628caa46
    Former-commit-id: 5a03342887a3fcb765c903080cd25afe6e5fd1d5

--- InformationTheory2015/Assignment02/problem1.hs
+++ InformationTheory2015/Assignment02/problem1.hs
@@ -1,56 +1,90 @@
 -- Solution to problem 1
 import Test.QuickCheck
+import Data.List 
+import Text.Printf
 
 -- Elem could be from X, Y for {X, Y}
 data Elem = X Int | Y Int | XY (Elem, Elem) deriving (Show, Eq, Ord)
 
 -- Probability Type for each element.
 data Prob a = P (Elem, a) deriving (Show, Eq, Ord)
+getP (P (elem, p)) = p
+getE (P (elem, p)) = elem
+prob_mat :: Fractional a => [[a]]
+
+prob_mat =  [ [1/4, 1/4], [0, 1/2] ]
+-- This is from book for testing.
+--prob_mat = [ [1/8, 1/16, 1/32, 1/32 ] , [1/16, 1/8, 1/32, 1/32 ]
+--    , [1/16, 1/16, 1/16, 1/16 ] , [1/4, 0, 0, 0 ] ]
 
 -- Here are my sets of X and Y. and whole universe!
-allX = map (\x -> X x)[0, 1]
-allY = map (\y -> Y y)[0, 1]
+allX = map (\x -> X x)[0..(length prob_mat)-1]
+allY = map (\y -> Y y)[0..(length $ head prob_mat)-1]
 
 -- Full XY set.
 xys :: [ Elem ]
 xys = [ XY (x, y) | x <- allX, y <- allY ]
 
--- probability that X = a and Y = b.
-joint_prob :: Fractional a => Elem -> a
-joint_prob (XY el) = case el of 
-     (X 0, Y 0) -> 1/4
-     (X 1, Y 0) -> 1/4
-     (X 0, Y 1) -> 0
-     (X 1, Y 1) -> 1/2
-     otherwise -> error $ "How did you reach here!? Not in our universe"
+
+joint_prob :: Fractional a => Elem -> Prob a
+joint_prob (XY el) = P (XY el, (prob_mat!!x)!!y) where (X x, Y y) = el
+
+-- Generic function to compute probability of any elem over the joint
+-- distribution joint_prob
+prob :: Fractional a => Elem -> Prob a
+prob (X a) = P (X a, sum [ getP $ joint_prob $ XY (X a, y) | y <- allY ])
+prob (Y a) = P (Y a, sum [ getP $ joint_prob $ XY (x, Y a) | x <- allX ])
+prob e = joint_prob e
 
 -- Probability distribution of X when Y = y or of Y when X = x.
-cond_prob :: Fractional a => Elem -> [Prob a]
-cond_prob (Y a) = normalize [ P (x, joint_prob $ XY (x, Y a)) | x <- allX ]
-cond_prob (X a) = normalize [ P (y, joint_prob $ XY (X a, y)) | y <- allY ]
-
+cond_prob_dist :: Fractional a => Elem -> [Prob a]
+cond_prob_dist (Y a) = normalize [ joint_prob $ XY (x, Y a) | x <- allX ]
+cond_prob_dist (X a) = normalize [ joint_prob $ XY (X a, y) | y <- allY ]
 normalize ps = map (\(P (x, y)) -> P (x, y/sum_ps)) ps where 
     sum_ps = sum $ map (\(P x) -> snd x) ps
 
--- which must satisfy the following.
-prop1_sum_equal_one = (sum $ map joint_prob xys) == 1.0
-
+-- This function computes the entropy 
+-- H = - \sum_{i \in N} p_i * log_2 p_1 
 entropy :: [ Prob Float ] -> Float
 entropy [] = 0.0
-entropy ((P (X x, p)):xs) = - p * (logBase 2 p) + (entropy xs)
-entropy ((P (Y x, p)):xs) = - p * (logBase 2 p) + (entropy xs)
+entropy ((P (elem, 0)):xs) = entropy xs
+entropy ((P (elem, p)):xs) = - p * (logBase 2 p) + (entropy xs) where 
 
 -- Compute H(X,Y)
-solve1 = 5
+-- Use entropy function over p(x, y)
+hxy = return $ entropy pxy where pxy = map (\x -> joint_prob x) xys
+hx = return $ entropy $ map prob allY
+hy = return $ entropy $ map prob allX
+hx_y = return $ sum $ map (\x -> ( getP . prob $ x) * ( entropy . cond_prob_dist $ x)) allX
+hy_x = return $ sum $ map (\x -> ( getP . prob $ x) * ( entropy . cond_prob_dist $ x)) allY
+
+solveF = do
+    x <- hx; y <- hy; xy <- hxy
+    return $ x + y - xy
+solveG = do
+    x <- hx; x_y <- hx_y 
+    return $ x - x_y
+solveH = do
+    y <- hy; y_x <- hy_x
+    return $ y - y_x
+solvei = solveF
+    
 
 -- All tests are here.
 tests = do
-    quickCheck prop1_sum_equal_one
+    quickCheck $ sum (map sum prob_mat) == 1.0
     putStrLn "Done checking"
 
 main = do
-    tests 
-    let ans1 = solve1
-    print $ "Ans1: " ++ show ans1
+    --tests 
+    hxy >>= printf "H(X,Y) = %f \n" 
+    hx >>= printf "H(X) = %f\n" 
+    hy >>= printf "H(Y) = %f\n"
+    hx_y >>= printf "H(X|Y) = %f\n"
+    hy_x >>= printf "H(Y|X) = %f\n"
+    solveF >>= printf "H(X) + H(Y) - H(X,Y) = %f\n"
+    solveG >>= printf "H(X) - H(X|Y) = %f\n"
+    solveH >>= printf "H(Y) - H(Y|X) = %f\n"
+    solvei >>= printf "I(X;Y) = H(X) - H(X,Y) = %f\n"
     putStrLn "Solved all"
 
