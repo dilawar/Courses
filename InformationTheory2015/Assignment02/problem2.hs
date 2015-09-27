11c6df14997b72b85831bd050a5d5094a74a0443
Dilawar Singh <dilawars@ncbs.res.in>
2015-09-27 22:56:12 +0530

    Not correct.
    Former-commit-id: 9e02a0bc898ee81e4f863b5e2da3a5fa6102c3b1
    Former-commit-id: 2c78c1ad7f953cf2bda0a567ca6221afe2ad1306

--- InformationTheory2015/Assignment02/problem2.hs
+++ InformationTheory2015/Assignment02/problem2.hs
@@ -7,7 +7,7 @@ data Balance = L | B | H deriving (Show,Eq,Ord)
 ncoins = 12
 states = 2*ncoins + 1
 
-p x | x > 0 = 1/x
+p x | x > 0 = 1/(fromIntegral x)
     | otherwise = error "Need a non-zero positive number"
 
 {-
@@ -16,10 +16,12 @@ balance n  = newstates n ++ states where
     rest_coins = ncoins - (2 * n)
 -}
 
+entropy :: [Double] -> Double
 entropy [] = 0.0
 entropy (0:xs) = entropy xs
 entropy (x:xs) = (- x * logBase 2 x) + entropy xs
 
+balance :: Int -> Balance -> [Double]
 balance 0 _ = error "Need more than 0 coins on each side"
 balance n B = map (\x -> 0.0) [1..2*n] ++ map (\x -> p (states-2*n)) [1..(states-2*n)] 
 balance n _ = map (\x -> p (2*n)) [1..2*n] ++ map (\x -> 0.0) [1..(states-2*n)] 
@@ -27,14 +29,14 @@ balance n _ = map (\x -> p (2*n)) [1..2*n] ++ map (\x -> 0.0) [1..(states-2*n)]
 xs = [ L, B, H]
 probs_x = [ 0.25, 0.5, 0.25 ]
 
-xys n = foldr (++) [] [map (\a -> a * (p (length xs))) $ balance n x | x <- xs]
+xys :: Int -> [Double]
+xys n = foldr (++) [] [map (\a -> a * (p $ length xs)) $ balance n x | x <- xs]
 
 hx = entropy probs_x
 hy n = sum $ map (\(x,y) -> y * (entropy $ balance n x)) (zip [L, B, H] probs_x)
 hxy n = entropy $ xys n
 
 mutual_info n = (hx + hy n) - (hxy n)
-
 
 main = do
     putStrLn "All done"
