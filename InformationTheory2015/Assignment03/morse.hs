4e43f8e3d5941b7e52e50bf2fbe0702b517d5fe7
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-05 19:11:06 +0530

    Added haskell script.
    Former-commit-id: 54882b645d5a464fd76d2a4879121e57929ff0b2
    Former-commit-id: 27449250f84732c30f82c1eab0b3cfdb2146e64a

--- InformationTheory2015/Assignment03/morse.hs
+++ InformationTheory2015/Assignment03/morse.hs
@@ -2,11 +2,12 @@
 
 import Data.Graph.Inductive 
 import Data.List
+import Control.Monad.Random
 
-data Symbol = DOT | DASH | LSPACE | WSPACE deriving (Show,Ord,Eq)
+data Symbol = DT | DS | LS | WS deriving (Show,Ord,Eq)
 type Prob = Double
 
-symbols = [ DOT, DASH, LSPACE, WSPACE ]
+symbols = [ DT, DS, LS, WS ]
 
 node_indices :: [Int]
 node_indices = [0..length symbols - 1]
@@ -17,12 +18,35 @@ transition_probs =  [
         , [1/2, 1/2, 0, 0], [1/2, 1/2, 0, 0]
     ]
 
-transitions = [ (x, y, transition_probs!!x!!y) | x <- node_indices, y <- node_indices ]
+transitions = [ (x, y, transition_probs!!x!!y) 
+        | x <- node_indices, y <- node_indices 
+    ]
 
 -- Gr (Node data type) (Edge data type)
 state_graph :: Gr Symbol Prob
 state_graph = mkGraph (zip node_indices symbols) transitions
 
+-- Get next_state from graph
+next_state :: MonadRandom m => Node -> m Int
+next_state state = do 
+    let weighted_list = map (\(a,b,p) -> (b, toRational  p)) $ out state_graph state
+    next <- fromList weighted_list 
+    return next
+
+rest_sequence xs 0 = return $ map (\x -> symbols!!x) xs
+rest_sequence xs n = do 
+    next <- next_state (head xs)
+    rest <- rest_sequence (next:xs) (n-1)
+    return rest
+
+-- Generate a sequence of n character
+gen_sequence n = do 
+    init_state <- uniform $ nodes state_graph
+    let s1 = symbols!!init_state
+    ns <- rest_sequence [init_state] n
+    return ns
+
 main = do
-    prettyPrint state_graph
+    s <- gen_sequence 1000
+    print s
     putStrLn "All done"
