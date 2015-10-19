fa4df2737e5c8608f96ede7aca548c9146d86903
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-19 15:46:16 +0530

    see https://github.com/ivan-m/graphviz/issues/3
    Former-commit-id: 29dbe0bd3f15355b2c62f96ef24e4c9074e7d404
    Former-commit-id: 4d546efcb663d126247dce59089c03349057e3cb

---
+++ InformationTheory2015/Assignment04/graphviz_bug/gv.hs
@@ -0,0 +1,15 @@
+import Data.GraphViz
+import Data.GraphViz.Commands.IO
+import Data.GraphViz.Types
+import Data.Graph
+import System.Environment
+import qualified Data.Text as T
+
+read_dot_file :: String -> IO (DotGraph String)
+read_dot_file filename = readDotFile filename
+
+main = do
+    files <- getArgs
+    dots <- mapM read_dot_file files
+    print $ dots
+    putStrLn "Done"
