e3f00b8d69de83c2b8ea813b713f0f72687d890a
Dilawar Singh <dilawars@ncbs.res.in>
2015-10-19 22:32:27 +0530

    I can now generate a random input sequence.
    Former-commit-id: 660fd86f5e3e2458d674719c24893a1d655cad3a
    Former-commit-id: b53a769e23c6af03caa0e99a2a2ed773cc8a32f5

--- InformationTheory2015/Assignment04/channel.hs
+++ InformationTheory2015/Assignment04/channel.hs
@@ -1,6 +1,7 @@
 import Data.List
 import Data.Maybe
 import qualified Numeric.Probability.Distribution as D
+import qualified Numeric.Probability.Random as DR
 
 type Symbol = Int
 
@@ -16,6 +17,9 @@ data Channel a = Channel { inSymbols :: [a]
 -- Let's construct our channel with input symbols (inputSymbols), output symbols
 -- (outSymbols) and probability of a inputSymbols going to outSymbols.
 inputSymbols = [0..10]
+inputDist :: D.T Float Symbol
+inputDist = D.uniform inputSymbols
+
 outputSymbols = [0..10]
 transitions = [ (x, y) | x <- inputSymbols, y <- map (\a -> mod a 11) [x+1..x+3] ]
 tranProbs = [ (a, 1/3) | a <- transitions ]
@@ -24,21 +28,23 @@ tranProbs = [ (a, 1/3) | a <- transitions ]
 channel1 :: Channel Symbol
 channel1 = Channel inputSymbols outputSymbols (D.fromFreqs tranProbs)
 
--- probs
---prob :: (Symbol, Symbol) -> Float
-prob x = (==fst x) D.?? (transitionProbs channel1) 
+-- Given a symbol find the next possible symbols.
+possibleOutputs :: Symbol -> [(Symbol, Float)]
+possibleOutputs x = map (\(e, p) -> (snd e, p)) $ filter (\(e, p) -> fst e == x) tranProbs
+
+-- Given a symbol, pick a one output symbol depending on the given
+-- probabilities.
+transition x = DR.run $ DR.pick $ D.fromFreqs $ possibleOutputs x
 
 --------------------------------------------------------------------------------
 -- Simulate channel
 --------------------------------------------------------------------------------
 
-{-
--- Apply a single input to the channel and get the output.
---input :: Symbol -> Channel -> Symbol
-input x ch = probOutput where 
-    probOutput = map (\(a, x) -> (snd a, x)) $ filter (\(a, y) -> fst a == x) $ transitionProbs ch
-    
--}
+inputSeq 0 = return []
+inputSeq n = do 
+    xv <- inputSeq (n-1)
+    x <- DR.run $ DR.pick $ inputDist
+    return $ (x : xv)
 
 main = do
     {-solve1 >>= print .show -}
