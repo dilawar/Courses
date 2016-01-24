5f5e0eb58a802fee89b8f1b96d7835e67c8525a0
Dilawar Singh <dilawars@ncbs.res.in>
2016-01-24 16:14:47 +0530

    I can now sample the space.
    Former-commit-id: 447d3b1f6755f5d0cfa74013bc0779f31ea50e2b
    Former-commit-id: 364f830422cb9f52bc03239b436e8581b209ed15

--- AdvancedComputationalMethods/Notes/monte_carlo_sampling.hs
+++ AdvancedComputationalMethods/Notes/monte_carlo_sampling.hs
@@ -4,16 +4,25 @@
  -}
 
 import qualified System.Random.MWC as R
-import Data.Vector.Unboxed
+import Data.Vector.Unboxed as U
 
 -- Very first thing we need is a random number generator (uniform distribution).
-random_floats :: Int -> IO (Vector Float)
+random_floats :: Int -> IO (U.Vector Float)
 random_floats n = R.create >>= \gen -> R.uniformVector gen n 
 
--- 
+-- To scale the random floats in (0, 1.0) to (min, max)
+-- y = (max - min) x + min 
+scale_samples :: (Float, Float) -> U.Vector Float -> U.Vector Float
+scale_samples (min, max) vs = U.map (\v -> (max - min) * v + min ) vs
+
+-- Sample a given space for n points.
+sampled_space :: [( Float, Float)] -> Int -> IO [ U.Vector Float ]
+sampled_space axes n = 
+    random_floats n >>= \vv -> return $ Prelude.map (\x -> scale_samples x vv ) axes 
+    
 
 main = do
-    vs <- random_floats 100
-    print $ vs 
+    vs <- sampled_space [ (1,3), (-1,1) ] 100
+    print vs
     putStrLn $ "Done"
 
