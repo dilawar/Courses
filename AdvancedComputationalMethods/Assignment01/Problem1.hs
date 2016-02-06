7e59f3a360884d1c1be263d9593191460b5afb85
Dilawar Singh <dilawars@ncbs.res.in>
2016-02-06 10:51:39 +0530

    Assignment 1.
    Former-commit-id: 9cd6bb9f73972cfa1e69db53bb00d08164fe7055
    Former-commit-id: eefb19b27ef620552db0df9acb6dbbd738ba29e1

--- AdvancedComputationalMethods/Assignment01/Problem1.hs
+++ AdvancedComputationalMethods/Assignment01/Problem1.hs
@@ -12,22 +12,34 @@ prg_linear_conguential a b m ri = next_num : prg_linear_conguential a b m next_n
 randoms a b m r0 n = take n $ prg_linear_conguential a b m r0
 
 -- Get the distinct elements from list with their count
-elements l = L.map (\x -> L.elemIndices x l) distinct 
+elements l = zip distinct $ L.map (\x -> L.elemIndices x l) distinct 
   where 
     distinct =  L.nub l
 
 -- Given an element x and a list ls, return the 
-is_1periodic ls x  = all_the_same . diff $ L.elemIndices x ls 
+is_1periodic_in :: (Eq a, Ord a) => [a] -> a -> (Int, Bool)
+is_1periodic_in [] x = (0, True)
+is_1periodic_in ls x  = (head $ periods ls x, all_the_same $ periods ls x)
 
+periods ls x
+    | length elements < 3 = error "There should be at least 3 element"
+    | otherwise = diff elements
+  where 
+    elements = L.elemIndices x ls 
+
+is_1periodic ls =  map (is_1periodic_in ls) (L.nub ls)
+
+diff [] = []
 diff (x:[]) = []
-diff (x:xs) = (head xs - x ) : diff xs 
+diff (x:xs) = (head xs - x) : diff xs 
 
+all_the_same [] = True
 all_the_same xs = and $ map (== head xs) (tail xs)
-
 
 random_number_generator_test = do
     let a = randoms 10 5 7 3 1000
-    let b = elements a
+    print $ a
+    let b = is_1periodic a
     print $ b
 
 main = do 
