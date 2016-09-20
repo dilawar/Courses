50830a9d97f8f7dabaf3f36f3ee2ad0721d20dbb
Dilawar Singh <dilawars@ncbs.res.in>
2016-09-20 18:40:44 +0530

    Both 2d and 3d plots are added.
    Former-commit-id: a222d7246cc8dd48e4e07f974f9fd66d12cef359
    Former-commit-id: 9714db80c5d9beb568a3be5a924c76178aa92c49

--- Calculus2016/Homework1/sol2_a.py
+++ Calculus2016/Homework1/sol2_a.py
@@ -5,17 +5,26 @@ import random
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def main():
-    xs = np.random.randint(-10000, 10000, 10**5)
-    ys = np.random.randint(-10000, 10000, 10**5)
+    fig = plt.figure()
+    ax = plt.axes( projection = '3d' )
+    xs = np.random.randint(-10000, 10000, 10**3)
+    ys = np.random.randint(-10000, 10000, 10**3)
     v1 = np.abs( np.abs(xs) - np.abs( ys ) )
     v2 = np.abs( xs + ys )
-    plt.plot( v1, v2,  '.' )
+    ax.scatter( xs, ys, v1, marker ='.' )
+    # ax.plot_wireframe( xs, ys, v1, color ='red' )
+    # ax.plot_trisurf( xs, ys, v1, color ='red', alpha = 0.4 )
+    ax.scatter( xs, ys, v2, marker = ',' )
+    # ax.plot_wireframe( xs, ys, v2, color = 'blue' )
+    # ax.plot_trisurf( xs, ys, v2, color = 'blue', alpha = 0.4 )
     plt.xlabel( '||x| - |y||' )
     plt.ylabel( '|x + y|' )
     plt.tight_layout( )
     plt.savefig( "%s.png" % sys.argv[0] )
+    # plt.savefig( "%s_3d.png" % sys.argv[0] )
     
 
 if __name__ == '__main__':
