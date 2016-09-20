e8dfe926d56f1931bee77f79810c8cbd1b8a8b79
Dilawar Singh <dilawars@ncbs.res.in>
2016-09-20 19:09:35 +0530

    Updated scripta and pdf file.
    Former-commit-id: b1f7f7bcb378b6b6eb985599bd13fbe40c8e8c78
    Former-commit-id: 36b797ea4def8acfacc4508a7330c899c7f9628f

---
+++ Calculus2016/Homework1/sol2_b.py
@@ -0,0 +1,24 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main( ):
+    for i in range( 5 ):
+        plt.subplot( 5, 1, i + 1)
+        # draw x randomly between -a to a, total N samples.
+        a = 2 ** (i + 2)
+        xs = np.random.random( 10000 )
+        xs = 2 * a * (xs - 0.5 )
+        res = []
+        for x in xs:
+            v = abs( (x**2.0) - 2.0 * x - 3.0)
+            if v > x:
+                res.append( x )
+        plt.plot( res, [1]*len(res), '|', label = '-%.1f < x < %.1f' % (a,a) )
+        plt.legend( )
+        plt.yticks( [], [] )
+        plt.xlabel( 'x' )
+    plt.savefig( '%s.png' % sys.argv[0] )
+
+if __name__ == '__main__':
+    main()
