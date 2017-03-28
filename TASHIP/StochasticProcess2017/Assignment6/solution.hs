a8300dc4540bf1e8803c070c537b93c045f6588c
Dilawar Singh <dilawars@ncbs.res.in>
2017-03-28 11:52:14 +0530

    updated script for different velocity variables.
    Former-commit-id: 2874d8d625a6de7e34cdfa78a2eb8b9546a4e3c7
    Former-commit-id: 059f7a5c14be4e8cc8e2c93385ecb7086aa9ee4c

--- TASHIP/StochasticProcess2017/Assignment6/solution.hs
+++ TASHIP/StochasticProcess2017/Assignment6/solution.hs
@@ -1,6 +1,7 @@
 -- Solution to problem 6.
 
 import Data.Random.Normal -- from normaldistribution
+import System.Random
 import Data.List
 import System.Environment
 
@@ -8,34 +9,42 @@ import System.Environment
 mByGamma = 1
 ktbyM = 1
 
+
+simulateUsingLangevian v0 steps dt = do 
+    g <- getStdGen
+    let alphas = take steps $ normals' (0, 1.0) g 
+    return $ simulate alphas 0.0 v0 0.0 dt
+
 -- Compute next v
 nextV alpha v dt = v - (v/mByGamma*dt) + alpha * ((2*ktbyM/mByGamma)**0.5) *(dt ** 0.5 )
 
-{-simulateUsingLangevian :: [ Float ]-}
-simulateUsingLangevian steps v0 dt = simulate alphas v0 0.0 dt
-  where 
-    alphas :: [ Double ]
-    alphas = take steps $ mkNormals' (0, 1.0) steps      -- Using steps as seed!
-
-simulate [ ] _ _ _ = [ ]
-simulate (alpha:alphas) v0 t0 dt = [t0+dt,nv] : simulate alphas nv (t0+dt) dt 
+-- Simulate
+simulate [ ] _ _ _ _ = [ ]
+simulate (a:as) x0 v0 t0 dt = [t0+dt,nv,nx] : simulate as nx nv (t0+dt) dt 
   where 
     nv :: Double
-    nv = nextV alpha v0 dt
+    nv = nextV a v0 dt
+    nx :: Double
+    nx = x0 + nv * dt
 
 
 -- Helper function. Write CSV file.
+lineToStr :: [ Double ] -> String
 lineToStr line =  intercalate "," $ map show line
 writeCsv datafile xs = do 
-    let text = "time,velocity\n" ++ (unlines $ map lineToStr xs)
+    let text = "time,velocity,position\n" ++ (unlines $ map lineToStr xs)
     writeFile datafile text
+
+toDouble :: String -> Double
+toDouble x = read x
 
 main = do
     args <- getArgs
-    -- Args are stop, step and outfile name.
-    let (stop:step:filename:[]) = args
-    let nSteps = floor ( (read stop) / (read step))
-    let trajectory = simulateUsingLangevian nSteps 0.0 0.1
+    -- Args are stop, step, v0 and outfile name.
+    let (stop:step:velocity:filename:[]) = args
+    let nSteps = floor $ (toDouble stop) / (toDouble step)
+    let v0 = toDouble velocity
+    trajectory <- simulateUsingLangevian v0 nSteps (toDouble step)
     -- Save trajectory to a CSV file
     writeCsv filename trajectory
     putStrLn $ "Wrote data to " ++ filename
