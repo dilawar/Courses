d007d892842d7673b661797be6bfc3b6259552b5
Dilawar Singh <dilawars@ncbs.res.in>
2017-04-06 20:11:52 +0530

    Assignment solution.
    Former-commit-id: 8826e0e5a198965d2bd3596844b98616538f5afb
    Former-commit-id: 08ddc25e123e20a69ba99ff41e1b8510fdafa7ae

--- TASHIP/StochasticProcess2017/Assignment7/solution.hs
+++ TASHIP/StochasticProcess2017/Assignment7/solution.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DuplicateRecordFields  #-}
+
 -- Solution to problem 6.
 
 import Data.Random.Normal -- from normaldistribution
@@ -9,23 +11,38 @@ import System.Environment
 mByGamma = 1
 ktbyM = 1
 
-gillespie step stop = do
+data Molecule = Molecule { name :: String, n0 :: Int } deriving Show 
+
+data Reac = Reac { name :: String
+                , subs :: [ Molecule ],  prds :: [ Molecule ]
+                , kf :: Double, kb :: Double 
+                } deriving Show 
+
+data System = System { name :: String, reacs :: [ Reac ] } deriving Show 
+
+a = Molecule "a" 100
+phi = Molecule "phi" 0
+rf = Reac "forward" [a] [phi] 1.0 0.0
+rb = Reac "backward" [phi] [a] 0.01 0.0
+
+-- Create a system.
+sys = System "Assign7" [rf, rb ]
+
+gillespie :: System -> Double -> Double -> IO ( System )
+gillespie sys dt t = do
     g <- getStdGen
-    let alphas = take (ceil $ stop / step) $ normals' (0, 1.0) g 
-    return $ simulate alphas 0.0 v0 0.0 dt
+    let n = floor ( t / dt )
+    let rs = take n $ normals' (0,1.0) g
+    step sys rs
 
--- Compute next v
-nextV alpha v dt = v - (v/mByGamma*dt) + alpha * ((2*ktbyM/mByGamma)**0.5) *(dt ** 0.5 )
+-- whichReac r sys = do 
+    
 
--- Simulate
-simulate [ ] _ _ _ _ = [ ]
-simulate (a:as) x0 v0 t0 dt = [t0+dt,nv,nx] : simulate as nx nv (t0+dt) dt 
-  where 
-    nv :: Double
-    nv = nextV a v0 dt
-    nx :: Double
-    nx = x0 + nv * dt
-
+step :: System -> [ Double ] -> IO System 
+step sys (r1:r2:rs) = do
+    reac <- whichReac r1 sys
+    return sys
+    
 
 -- Helper function. Write CSV file.
 lineToStr :: [ Double ] -> String
@@ -40,10 +57,12 @@ toDouble x = read x
 main = do
     args <- getArgs
     -- Args are stop, step, v0 and outfile name.
-    let (stop:step:filename:[]) = args
-    let nSteps = floor $ (toDouble stop) / (toDouble step)
-    let v0 = toDouble velocity
-    trajectory <- gillespie step stop
+    let (t:dt:filename:[]) = args
+    let step = toDouble dt
+    let stop = toDouble t
+    trajectory <- gillespie sys step stop
+    print $ trajectory
     -- Save trajectory to a CSV file
-    writeCsv filename trajectory
-    putStrLn $ "Wrote data to " ++ filename
+    -- writeCsv filename trajectory
+    -- putStrLn $ "Wrote data to " ++ filename
+    putStrLn $ "All done"
