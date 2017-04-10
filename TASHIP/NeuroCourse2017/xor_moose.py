4afb5f65afba59cff70feb52fd373b8bca86f677
Dilawar Singh <dilawars@ncbs.res.in>
2017-04-10 15:30:12 +0530

    Updated solution.
    Former-commit-id: 77dd1d7bb46f76cac1290a088cd854c3f84451f5
    Former-commit-id: 073f63a83cf7e5a239963727ea5db9d8266934c6

---
+++ TASHIP/NeuroCourse2017/xor_moose.py
@@ -0,0 +1,73 @@
+import moose
+import moose.utils as mu
+import numpy as np
+import pylab
+
+def xor( a, b ):
+    compt = moose.CubeMesh( '/compt' )
+    compt.volume = 1
+    species = {}
+    tables = { }
+    concInit  =  { 'a' : a, 'b' : b, 'r' : 1, 'x' : 1 }
+
+    for s in [ 'a', 'b', 'r', 'x', 'x*', 'ab', 'xab' ] :
+        p = moose.Pool( '%s/%s' % (compt.path, s) )
+        t = moose.Table2( '%s/%s_table' % (compt.path, s ) ) 
+        moose.connect( t, 'requestOut', p, 'getConc' )
+        tables[ s ] = t
+        p.concInit = concInit.get( s, 0.0 )
+        species[ s ] = p
+
+    # Now the reactions.
+    r1 = moose.Reac( '%s/reac1' % compt.path )
+    moose.connect( r1, 'sub', species[ 'a' ], 'reac' )
+    moose.connect( r1, 'sub', species[ 'b' ], 'reac' )
+    moose.connect( r1, 'prd', species[ 'ab' ], 'reac' )
+    r1.Kf = 1
+    r1.Kb = 0
+
+    r2 = moose.Reac( '%s/reac2' % compt.path )
+    moose.connect( r2, 'sub', species[ 'r' ], 'reac' )
+    moose.connect( r2, 'sub', species[ 'x' ], 'reac' )
+    moose.connect( r2, 'prd', species[ 'x*' ], 'reac' )
+    moose.connect( r2, 'prd', species[ 'r' ], 'reac' )
+    r2.Kf = 1
+    r2.Kb = 0
+
+    r3 = moose.Reac( '%s/reac3' % compt.path )
+    moose.connect( r3, 'sub', species[ 'x*' ], 'reac' )
+    moose.connect( r3, 'sub', species[ 'ab' ], 'reac' )
+    moose.connect( r3, 'prd', species[ 'xab' ], 'reac' )
+    r3.Kf = 1
+    r3.Kb = 0.01
+
+    stoich = moose.Stoich( '%s/stoich' % compt.path )
+    ksolve = moose.Ksolve( '%s/ksolve' % compt.path )
+    stoich.ksolve = ksolve
+    stoich.compartment = compt
+    stoich.path = '%s/#' % compt.path
+
+    moose.reinit( )
+    moose.start( 20 )
+    return tables['x*'].vector[-1]
+
+def main( ):
+    img = [ ]
+    for i in range( 0, 10, 1 ):
+        row = [ ]
+        for j in range( 0, 10, 1 ):
+            out = xor( i, j)
+            row.append( out  )
+            moose.delete( '/compt' )
+        img.append( row )
+
+    print( img )
+    pylab.imshow( img, interpolation = 'none' )
+    pylab.colorbar( )
+    pylab.title( 'XOR gate' )
+    pylab.xlabel( 'A' )
+    pylab.ylabel( 'B' )
+    pylab.savefig( 'xor.png' )
+
+if __name__ == '__main__':
+    main()
