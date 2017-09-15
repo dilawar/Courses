8880544a10b83b9f440a17531dd24d740fee64d8
Dilawar Singh <dilawars@ncbs.res.in>
2017-09-15 18:53:44 +0530

    Updated problem.
    Former-commit-id: 4ecb37191707a0f996490ad35255fd1531169ebb
    Former-commit-id: 89eed92858947f7f6d23c26cf895f6fd097c25cf

--- TASHIP/InformationTheory2017/HW1/problem1.hs
+++ TASHIP/InformationTheory2017/HW1/problem1.hs
@@ -1,12 +1,19 @@
 import qualified Data.List as L
 
-probs k = map (\i -> 2 ** (-i) ) [1..k]
 
-problem1 = helper' 8 5
 
-space n = [ (a,b,c) | a <- [0,1..n], b <- [0,1..n-a], c <- [0,1..n-a-b] ]
+probs = map (\i -> 2 ** (-i) ) [1..5]
+space n = [ 
+    [a,b,c,d,e] | a <- [0,1..n]
+    , b <- [0,1..n-a]
+    , c <- [0,1..n-a-b]
+    , d <- [0,1..n-a-b-c] 
+    , e <- [0,1..n-a-b-c-d] 
+    ]
 
-helper' n k = sequence $ L.replicate k allopts
+allProbs [] [] = 0.0
+allProbs (x:xs) (p:ps) = x * p + allProbs xs ps
+
+problem1 = filter (\b -> sum b == 8.0) $ filter (\a -> allProbs a probs == 1.0) $ allSpace 
   where 
-    allopts = [0,1..n]
-    {- pbs = probs k -}
+    allSpace = space 8
