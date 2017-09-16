1a48fa4bf4471c9c1b5654e6b22ee63f34e87e2e
Dilawar Singh <dilawars@ncbs.res.in>
2017-09-16 10:43:14 +0530

    Added haskell script to solve the problem.
    Former-commit-id: 0a2206e1e30883da94a48e1ea59525ade8c79ca9
    Former-commit-id: c5801f7a37edb8845d5f8d964321407549b0ba92

--- TASHIP/InformationTheory2017/HW1/problem1.hs
+++ TASHIP/InformationTheory2017/HW1/problem1.hs
@@ -1,29 +1,40 @@
 import qualified Data.List as L
 
-
 tf = fromIntegral
 
 probs :: Floating a => Int -> [a]
 probs k = map (\i -> 2.0 ^^ (-i) ) [1..k]
 
+probSpace = probs 5
+
+plogp p = p * (logBase 2.0 p)
+
 space :: Int -> [ [Int] ]
-space n = [ 
-    [a,b,c,d,e] | a <- [0,1..n]
+space n = [ [a,b,c,d,e] | a <- [0,1..n]
     , b <- [0,1..n-a]
     , c <- [0,1..n-a-b]
     , d <- [0,1..n-a-b-c] 
-    , e <- [0,1..n-a-b-c-d] 
+    , e <- [n-a-b-c-d] 
     ]
 
-allProbs :: [Int] -> [Float] -> Float
-allProbs [] [] = 0.0
-allProbs (x:xs) (p:ps) = (tf x * p) + allProbs xs ps
 
-computeEntropy :: Floating a => [a] -> [a] -> a
+sumProb :: [Int] -> [Float] -> Float
+sumProb [] [] = 0.0
+sumProb (x:xs) (p:ps) = (tf x * p) + sumProb xs ps
+
+{- computeEntropy :: Floating a => [a] -> [a] -> a -}
 computeEntropy [ ] [ ] = 0.0
-computeEntropy (x:xs) (p:ps) = - x * p * (logBase 2.0 p) + computeEntropy xs ps
+computeEntropy (x:xs) (p:ps) = - (tf x) * (plogp p) + computeEntropy xs ps
 
-problem1 = computeEntropy sol (probs 5) 
+problem1 = map (\s -> (s, computeEntropy s probSpace) ) sol
+    {- ( sol, probSpace ) -}
   where 
-    sol = filter (\b -> sum b == 8) $ filter (\s -> (allProbs s (probs 5)) == 1.0) $ allSpace 
+    sol = filter (\s -> (sumProb s probSpace) == 1.0) $ allSpace 
     allSpace = space 8 
+
+printLine (xs, p ) = L.intercalate " | " (map show xs) ++ " | " ++ show p
+
+main = do
+    let sol1 = problem1 
+    let p = L.intercalate "\n" (map printLine sol1) 
+    putStrLn p
