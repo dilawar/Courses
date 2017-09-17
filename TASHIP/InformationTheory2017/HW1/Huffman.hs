5778480fd5f8dbe37122ff00b305013d1be4ee38
Dilawar Singh <dilawars@ncbs.res.in>
2017-09-17 19:02:14 +0530

    Updated.
    Former-commit-id: efe1f7f78a417c61cbf3a4ea027782de5e7b5033
    Former-commit-id: 903b96eec974035bd77adb786a738728f9777565

--- TASHIP/InformationTheory2017/HW1/Huffman.hs
+++ TASHIP/InformationTheory2017/HW1/Huffman.hs
@@ -1,17 +1,38 @@
 module Huffman where 
 
 import qualified Data.List as L
+import qualified Data.Graph as G
 
-data HTree = Branch HTree HTree | Leaf Int deriving (Show, Eq, Ord)
+data Bit = Zero | One deriving (Show,Ord,Eq)
 
-huffman ps = huffman' (L.length ps)
+-- Huffman tree. 
+data HTree = Branch Double (HTree, HTree) | Leaf  Double deriving (Show, Eq, Ord)
 
-huffman' 0 = Leaf 0
-huffman' n = Branch (huffman' (n-1)) (huffman' (n-1))
+nodeVal (Branch x (l,h)) = x
+nodeVal (Leaf x) = x
+
+compareHt x1 x2 = compare (nodeVal x1 ) (nodeVal x2)
+
+-- Convert HTree to String.
+toString (Leaf d) = show d
+toString (Branch p (l,r)) = "<" ++  toString l ++ "," ++ toString r ++ ">"
+
+-- Step: merge pair of HTree into one HTree
+step [ ] = []
+step (h:[]) = [h]
+step (h1:h2:hs) = (Branch (nodeVal h1 + nodeVal h2) (h1, h2)) : merge hs
+
+merge hs | (length hs) > 1 = merge $ step hs -- merge $ L.sortBy compareHt (step hs)
+         | otherwise = hs
 
 -- Test function.
-test x = huffman $ L.sort x
+test x = merge initHuffman 
+  where 
+    initHuffman = map (\a -> Leaf a) $ L.sortBy (compare) x
 
 main = do
-    let res = test $ [ 1/2, 1/4 ] ++  replicate 2 (1/16)  ++ replicate 4 (1/32)
-    print $ res
+    let probs = [ 1/2, 1/4 ] ++ replicate 2 (1/16)  ++ replicate 4 (1/32)
+    print $ probs
+    let res = test probs 
+    print $ toString $ head res
+    putStrLn "All done"
--- TASHIP/InformationTheory2017/HW1/problem1.hs
+++ TASHIP/InformationTheory2017/HW1/problem1.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE BangPatterns #-}
+
 import qualified Data.List as L
+
+import Data.Compression.Huffman  -- cabal install huffman
 
 tf = fromIntegral
 
@@ -9,6 +13,8 @@ probSpace = probs 5
 
 plogp p = p * (logBase 2.0 p)
 
+sortCode (i1, c1) (i2, c2) = compare i1 i2 
+
 space :: Int -> [ [Int] ]
 space n = [ [a,b,c,d,e] | a <- [0,1..n]
     , b <- [0,1..n-a]
@@ -17,6 +23,10 @@ space n = [ [a,b,c,d,e] | a <- [0,1..n]
     , e <- [n-a-b-c-d] 
     ]
 
+
+avg l = (tf $ sum l) / (tf $ length l)
+
+codeToStr code = L.intercalate "" $ map show code
 
 sumProb :: [Int] -> [Float] -> Float
 sumProb [] [] = 0.0
@@ -32,9 +42,27 @@ problem1 = map (\s -> (s, computeEntropy s probSpace) ) sol
     sol = filter (\s -> (sumProb s probSpace) == 1.0) $ allSpace 
     allSpace = space 8 
 
-printLine (xs, p ) = L.intercalate " | " (map show xs) ++ " | " ++ show p
+toProbs a i =  replicate a (probSpace!!i) 
+
+printLine (xs, p) = L.intercalate " | " (map show xs) ++ " | " ++ show p
+
+printCode [ ] = ""
+printCode ((i,c):xs) = show i ++ " : " ++ codeToStr c ++ "\n" ++ printCode xs
 
 main = do
     let sol1 = problem1 
     let p = L.intercalate "\n" (map printLine sol1) 
     putStrLn p
+
+    -- huffman code.
+    let pbs = map ( \(x,p) -> zip [0..] (concat $ zipWith toProbs x [0..])) sol1
+    let codes = map (codewords . huffman) pbs
+    let sortedCodes = map (\l -> L.sortBy sortCode l) codes 
+    let codesStr = L.intercalate "\n" $ map printCode $ sortedCodes
+    let r = map (snd . unzip) sortedCodes
+    let cs = map  (\l -> map length l) r
+    -- print $ map avg cs
+    -- putStrLn codesStr
+    print cs
+    putStrLn "All done"
+
