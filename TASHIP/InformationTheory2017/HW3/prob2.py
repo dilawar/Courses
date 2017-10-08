7b1cc3d6df669273dd076b44cb730486fc41d2b7
Dilawar Singh <dilawars@ncbs.res.in>
2017-10-08 18:27:53 +0530

    Added python script.
    Former-commit-id: 37638ac52fab3ef7dc8234618909e4b7958688c6
    Former-commit-id: 2c7f3085a16df39fd97002782214137186520fea

---
+++ TASHIP/InformationTheory2017/HW3/prob2.py
@@ -0,0 +1,71 @@
+"""prob2.py: 
+
+"""
+    
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2017-, Dilawar Singh"
+__version__          = "1.0.0"
+__maintainer__       = "Dilawar Singh"
+__email__            = "dilawars@ncbs.res.in"
+__status__           = "Development"
+
+import sys
+import os
+import itertools
+import random
+import operator
+from functools import reduce
+from collections import Counter
+
+dist_ = { 'A' : 1/2.0, 'T' : 1/4.0, 'G' : 1/8.0, 'C' : 1/8.0 }
+vars_ = dist_.keys( )
+
+def seqToStr( seq ):
+    return ''.join( seq )
+
+def seq_prob( seq ):
+    prob = reduce( operator.mul, [ dist_[x] for x in seq ] )
+    return prob
+
+def is_typical( seq ):
+    """In a typical seq, letters occur as expected
+    """
+    global dist_
+    dist = Counter( seq )
+    isTypical = True
+    for v in vars_:
+        if dist.get(v, 0.0) / len(seq) != dist_[v]:
+            isTypical = False
+    return isTypical
+
+def typical( size = 8 ):
+    seqs = list( itertools.product( vars_ , repeat = size ) )
+    seqWithProbs = [ ]
+    typicalSeqs = [ ]
+    for s in seqs:
+        seqWithProbs.append( (seq_prob(s), s ) )
+        if is_typical( s ):
+            typicalSeqs.append( s )
+
+    seqWithProbs.sort( reverse = True )
+    print( 'Singal most probable seq        : %s' % seqToStr(seqWithProbs[0][1]) )
+
+    typProb = 0.0
+    if len( typicalSeqs ) > 0:
+        typProb = seq_prob( typicalSeqs[0] )
+
+    print( 'Probability of a typical seq    : %f' % typProb )
+    print( 'Number of typical seqs          : %d' % len( typicalSeqs) )
+    print( 'Probability of getting typical  seq: %f' % ( 
+        len( typicalSeqs ) / len( seqs ))
+        )
+
+def main( ):
+    for size in [ 4, 6, 8, 10, 12, 14 ]:
+        print( 'Seq size %d' % size )
+        typical( size )
+        print( "" )
+
+
+if __name__ == '__main__':
+    main()
