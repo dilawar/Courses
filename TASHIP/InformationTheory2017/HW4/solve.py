dbd6763dad6b14a97aa8a7abd5a739e7b3e1bebb
Dilawar Singh <dilawars@ncbs.res.in>
2017-10-16 13:50:36 +0530

    Waiting for Mukund's reply.
    Former-commit-id: ec24eacd3e8c8a8774dac9a904cc73a96b236508
    Former-commit-id: 9ee636a7c17324c1036e85fb8f4a13b4677fef58

--- TASHIP/InformationTheory2017/HW4/solve.py
+++ TASHIP/InformationTheory2017/HW4/solve.py
@@ -1,6 +1,7 @@
 """solve.py: 
 
 """
+from __future__ import print_function, division
     
 __author__           = "Dilawar Singh"
 __copyright__        = "Copyright 2017-, Dilawar Singh"
@@ -18,9 +19,9 @@ import matplotlib.tri as tri
 from collections import Counter, defaultdict
 import scipy.special
 
-fac = math.factorial
+fac = scipy.special.factorial
 
-N = 16
+N = 32
 
 def plot_ternary( pts, v, ax = None ):
     # translate the data to cartesian corrds
@@ -47,7 +48,7 @@ def plot_ternary( pts, v, ax = None ):
     trimesh = refiner.refine_triangulation(subdiv=4)
 
     #plotting the mesh
-    ax.triplot(trimesh, ',')
+    ax.triplot(trimesh, '.', alpha = 0.3 )
     ax.axis( 'off' )
 
     return ax
@@ -67,9 +68,9 @@ def compute_ternary( seqs ):
         probs[ tuple(pbs) ] += 1
     return probs
 
-def sequence( probs ):
+def prob_num_seq( probs ):
     global N
-    a, b, c = probs
+    a, b,  c = probs
     ea, eb, ec = [ N * x for x in probs ]
     na, nb, nc = [ int(N*x) for x in probs ]
     nc = N - na - nb
@@ -77,8 +78,23 @@ def sequence( probs ):
     ks = [ na, nb, nc ]
     nTS = np.prod( scipy.special.comb( ns, ks ) )
     pTS = a**na * b**nb * c**nc
-    print( '%.5g, %.5g, %.5g' % (nTS, pTS, nTS * pTS) )
+    return pTS, nTS
+
+def sequence( probs ):
+    global N
+    pTS, nTS = prob_num_seq( probs )
+    #print( '%.5g, %.5g, %.5g' % (nTS, pTS, nTS * pTS) )
     return nTS * pTS
+
+def typical_sequence( probs, pRef ):
+    global N
+    ps = np.prod( np.array( pRef ) ** ( np.array( probs ) * N ))
+    ns = [ np.rint(N*x) for x in probs ]
+    ns[-1] = ns[-1] + N - sum( ns )
+    assert sum( ns ) == N
+    ns = fac( N ) / np.prod( [ fac(x) for x in ns ] )
+    return ps, ns
+
 
 def main( ):
     points, vals = [ ], [ ]
@@ -95,11 +111,10 @@ def main( ):
     ax1 = plt.subplot( 121 )
     # for given probs generate many seqs
     pbs = [ 1/2, 1/3, 1/6 ]
+
     nseq = 10000
     seqs = generate_seqs( pbs, n = nseq )
-
     data = compute_ternary( seqs )
-
 
     pts, vals = list(data.keys( )), list(data.values( ))
     plot_ternary( pts, vals, ax1 )
@@ -108,8 +123,20 @@ def main( ):
             , arrowprops = dict( facecolor='black', arrowstyle='->' )
             )
 
+    # Here we find what values of a, b, and c gives us typical sequence.
     ax2 = plt.subplot( 122 )
-    probs = [ math.log( x / sum(vals), 2) for x in vals ]
+    pts, nts = prob_num_seq( [1/2, 1/3, 1/6] )
+
+    dd = { }
+    for a in np.arange(0, 1.1, 0.01):
+        for b in np.arange(1.0-a, -0.01, -0.01):
+            c = 1.0 - a - b
+            probs = (a, b, c)
+            ps, ns = typical_sequence( probs, [1/2, 1/3, 1/6] )
+            res = ps * ns
+            dd[ (a,b,c) ] = math.log( res )
+
+    pts, probs = list( dd.keys() ), list( dd.values( ) )
     plot_ternary( pts, probs, ax2 )
 
     plt.suptitle( 'N=%d, Length=%d' % (nseq,N) )
@@ -118,4 +145,4 @@ def main( ):
 
 
 if __name__ == '__main__':
-    main()
+    main( )
