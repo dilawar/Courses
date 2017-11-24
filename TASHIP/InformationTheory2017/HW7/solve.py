a160a3fb388c0e89cfa23ecebbe4c73e076afa33
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-24 17:04:04 +0530

    let the script run.
    Former-commit-id: 0ffcb641a3d1788d46a1847dce24f57f9467d082
    Former-commit-id: 3c71d5cfbc186ef219caa0aa86da1280e828d04e

--- TASHIP/InformationTheory2017/HW7/solve.py
+++ TASHIP/InformationTheory2017/HW7/solve.py
@@ -12,6 +12,7 @@ __email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
 import sys
+import time
 import os
 import numpy as np
 
@@ -23,17 +24,21 @@ def twoVsOne( vec ):
     return nTwo / nOne
 
 def main( ):
-    N = 10 ** 4
     nRare = 0.0
-
     # Run till 100 rare events are found.
+    t = time.time( )
+    N = 0
     while nRare < 100.0:
         r = roll_die( 1000 )
+        N += 1
         if twoVsOne( r ) >= 2.0:
             nRare += 1.0
             print( 'Ah, A rare event' )
+        if N % 100000 == 0:
+            print('+ Total Exp=%d. Time taken=%f.' % ( N, time.time() - t) )
 
     print( "Prob of rare event: %f" % (nRare / N ) )
+    print( 'Time taken %f seconds' % (time.time() - t ) )
 
 
 if __name__ == '__main__':
