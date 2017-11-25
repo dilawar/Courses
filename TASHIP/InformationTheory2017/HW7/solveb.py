f07c62ec9f78133f0d2237d2d92728ffcdd28800
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-25 19:12:37 +0530

    needs proof.
    Former-commit-id: 7844607266bfbf532813b2b2eead01fe5aeef0e6
    Former-commit-id: 4d81a20c129ed1cfb1b3dd1dc9366a539533bfca

--- TASHIP/InformationTheory2017/HW7/solveb.py
+++ TASHIP/InformationTheory2017/HW7/solveb.py
@@ -16,6 +16,10 @@ import os
 import random
 import math
 import numpy as np
+import PyGnuplot as pg
+
+def entropy( ps ):
+    return sum([ - math.log( p, 2.0 ) * p for p in ps ])
 
 def generate_probs( ):
     base = 1/6.0
@@ -28,24 +32,34 @@ def generate_probs( ):
         delta = random.random( ) / 100.0
         p[1] += delta
         p[ random.choice( [0,2,3,4,5] ) ] -= delta
+
+    # renormalize
+    p = p / sum( p )
+    assert p.all( ) > 0.0
     return list(p)
 
 def KL_divergence( P, Q ):
     dist = 0.0
     for p, q in zip( P, Q):
         dist -= p * math.log( q / p )
+    assert dist >= 0.0, (dist, P, Q)
     return dist
 
 def main( ):
     Q = [1/6.0] * 6
     results = [ ]
-    for i in range( 1000 ):
-        P = generate_probs( )
-        d = KL_divergence( P, Q )
-        results.append( (P, d ) )
+    with open( "tmp.dat", "w" ) as  f:
+        for i in range( 10000 ):
+            P = generate_probs( )
+            d = KL_divergence( P, Q )
+            results.append( (P, d, entropy(P) ) )
+            f.write( "%g %g\n" % (d, entropy(P)) )
 
-    results.sort( )
-    print( results[-1] )
+    pg.c( 'set xlabel "H(E)"' )
+    pg.c( 'set ylabel "D(E||Q), Q is uniform"' )
+    pg.c( 'set terminal pdf; set output "problem2.pdf"; ' )
+    pg.c( 'plot "tmp.dat" using 1:2 w p title "KL distance V/S entropy"' )
+
 
 if __name__ == '__main__':
     main()
