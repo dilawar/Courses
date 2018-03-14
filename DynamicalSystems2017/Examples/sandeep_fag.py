5a24dae90fadff1ecc20733c63444725304bc534
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-14 16:58:31 +0530

    Rest of the solver wet their pants.
    Former-commit-id: d27226b83ed21fa738037b207ab55fbf6dc98884
    Former-commit-id: ff52527c8f8b4755d0ea3337af302200d8a0a4b5

--- DynamicalSystems2017/Examples/sandeep_fag.py
+++ DynamicalSystems2017/Examples/sandeep_fag.py
@@ -1,5 +1,4 @@
 """sandeep_fag.py: 
-
 """
     
 __author__           = "Dilawar Singh"
@@ -12,29 +11,59 @@ __status__           = "Development"
 import sys
 import os
 import matplotlib.pyplot as plt
+import time
 import numpy as np
 import scipy.integrate
 
-def plot( ts, ys, outfile ):
-    plt.plot( ts, ys[:,0], label = 'x' )
-    plt.plot( ts, ys[:,1], label = 'y' )
-    plt.legend( )
-    plt.savefig( outfile )
-    print( '[INFO] Saved to %s' % outfile )
+def plot_data( ts, ys, title, outfile = None, ax = None ):
+    if ax is None:
+        ax = plt.subplot( 111 )
 
-def system(ss, t):
+    ax.plot( ts, ys[:,0], label = 'x' )
+    ax.plot( ts, ys[:,1], label = 'y' )
+    ax.legend( )
+    ax.set_title( title )
+
+    if outfile is not None:
+        plt.savefig( outfile )
+        plt.close( )
+        print( '[INFO] Saved to %s' % outfile )
+
+def system(t, ss):
     x, y = ss
     return [ x*(1-x)-x*y, 100*x*y - 0.1*y ]
 
-def lsoda( ts, init ):
-    solver_ = 'lsoda'
-    ys = scipy.integrate.odeint( system, init, ts )
-    plot( ts, ys, '%s_%s.png' % ( sys.argv[0], solver_ ) )
+def solve( ts, init, solver ):
+    t = time.time( )
+    r = scipy.integrate.ode( system )
+    r.set_initial_value( init, 0 )
+    r.set_integrator( solver )
+    dt = ts[1] - ts[0]
+    solution = [ ]
+    while r.successful( ) and r.t < ts[-1]:
+        r.integrate( r.t + dt )
+        solution.append( r.y )
+    return np.array( solution ), time.time() - t
+
+def other_solvers( ts, init ):
+    plt.figure( )
+    solvers = [ 'vode', 'zvode', 'lsoda', 'dopri5', 'dop853' ]
+    for i, solver in enumerate( solvers ):
+        ax = plt.subplot( int(len(solvers)/2)+1, 2, i+1 )
+        print( 'Solving using %s' % solver )
+        ys, timeTaken = solve( ts, init, solver )
+        title = '%s Time=%.4f sec' % (solver, timeTaken)
+        plot_data( ts[:len(ys)], ys, title=title, ax = ax )
+
+    plt.tight_layout( )
+    plt.savefig( '%s.png' % sys.argv[0] )
+    plt.close( )
+
 
 def main( ):
-    init = [100, 50]
-    ts = np.arange( 0, 1000, 0.1 )
-    lsoda( ts, init )
+    init = [10, 10]
+    ts = np.arange( 0, 100, 0.1 )
+    other_solvers( ts, init )
 
 if __name__ == '__main__':
     main()
