1119851cebffec3e65f52757a1f3bbd418194bc0
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-20 17:59:55 +0530

    Now I need to compute trajectories near these points.

--- DynamicalSystems2018/HW1/pendulum_pyds.py
+++ DynamicalSystems2018/HW1/pendulum_pyds.py
@@ -19,6 +19,7 @@ import math
 import random
 import itertools
 import PyDSTool as pd
+from PyDSTool.Toolbox import phaseplane as pp
 
 def construct_system( I, alpha, init=(0,0), T = 5 ):
     theta0, w0 = init
@@ -44,26 +45,34 @@ def plot( trajs, ax ):
     for t in trajs:
         x, y = t['theta'], t['w']
         dx, dy = x[1] - x[0], y[1] - y[0]
-        ax.plot( x, y, color = 'blue', lw = 0.5 )
+        ax.plot( x, y, '-.', color = 'blue', lw = 0.4 )
         ax.set_xlabel( r'$\theta$' )
         ax.set_ylabel( r'$\omega$' )
         ax.set_xlim( [ -2 *math.pi, 2 *math.pi ] )
 
 def compute_phase_plane( ode, I, alpha, ax = None ):
     ode.set( pars = dict( I = I, alpha = alpha ) )
+    ode.set( xdomain = dict( theta = [-2*math.pi,2*math.pi], w = [-math.pi, math.pi] ) )
     print( '| Computing for I=%f and alpha=%f' % (I,alpha) )
-    trajs = compute( ode )
+    trajs = None #compute( ode )
     if ax is None:
         return 
-    plot( trajs, ax)
+    if trajs:
+        plot( trajs, ax)
+    else:
+        # Find fixed-points and plot phase-plots.
+        fp_coord = pp.find_fixedpoints( ode, eps=1e-8 )
+        for p in fp_coord:
+            ax.plot( p['theta'], p['w'], 'o', color = 'blue' )
+        pp.plot_PP_vf( ode, 'theta', 'w', N = 20, scale_exp = -0.5 )
     ax.set_title( r'I=%.2f, $\alpha=%.2f$' % (I, alpha), fontsize = 8 )
 
 
 def main( args = None ):
     print( 'Constructing system' )
     ode = construct_system( 0.0, 0.0 )
-    ps = [ 0.0, 0.01, 0.1, 0.5, 1.0 ]
-    plt.figure( figsize = (len(ps)*2, len(ps)*2 ) )
+    ps = [ 0.0, 0.01, 0.1 ]
+    plt.figure( figsize = (len(ps)*3, len(ps)*3 ) )
     for i, (I, alpha) in enumerate(itertools.product(ps, repeat = 2)):
         ax = plt.subplot( len(ps), len(ps), i + 1 )
         compute_phase_plane( ode, I=I, alpha=alpha, ax = ax )
