9575c3270a4ef3be689250dfea00192a078df3d4
Dilawar Singh <dilawars@ncbs.res.in>
2018-04-20 11:12:49 +0530

    Flashing fireflies.

--- DynamicalSystems2018/Project/src/main.ino
+++ DynamicalSystems2018/Project/src/main.ino
@@ -13,69 +13,130 @@
 
 #include "LEDADD.h"
 
-int clockPin = 5;  // Connected to SH_CP (pin 11) of 74HC595
+#define NUM_RING        6
+#define NUM_LED_IN_RING 6
+#define NUM_LED         36
+
 int latchPin = 6;  // Connected to ST_CP (pin 12) of 74HC595
+int clockPin = 5;  // Connected to SH_CP (pin 11) of 74HC595
 int dataPin  = 7;  // Connected to DS (pin 14) of 74HC595
+// MAKE SURE YOU CHANGE THESE TO SUIT WHICH PINS YOU HAVE USED!
+
+
+int row;
+int col;
+
+byte Byte[5] ={};
+
+size_t ring_ = 0;
+size_t led_  = 0;
+
+bool status_[36] = {0};
+long last_active_[36] = {0};
 
 void setup()
 {
-    Serial.begin( 38400 );
 
-    pinMode(latchPin, OUTPUT);
-    pinMode(clockPin, OUTPUT);
-    pinMode(dataPin, OUTPUT);
+  Serial.begin(38400);  
+  
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
 
-    digitalWrite(latchPin, HIGH);
-    digitalWrite(clockPin, HIGH); 
+  digitalWrite(latchPin, HIGH);
+  digitalWrite(clockPin, HIGH); 
+ 
 }
 
-void writeData( byte val )
-{
-    digitalWrite( latchPin, LOW );
-    shiftOut( dataPin, clockPin, MSBFIRST, val );
-    digitalWrite( latchPin, HIGH );
-}
-
-void turnON( size_t groupnum, size_t lednum, size_t wait = 100 )
+void LedON(int row, int col)
 {
 
-    String msg( "LED " );
-    msg += String( groupnum );
-    msg += String( lednum );
+    row--;
+    col--;
 
-    for (size_t i = 0; i < 5; i++) 
+    Byte[0] = (Byte[0] | LED[row][col][0]);
+    Byte[1] = (Byte[1] | LED[row][col][1]);
+    Byte[2] = (Byte[2] | LED[row][col][2]);
+    Byte[3] = (Byte[3] | LED[row][col][3]);
+    Byte[4] = (Byte[4] | LED[row][col][4]);
+
+    for(int i=0 ; i<5 ;i++)
     {
-        byte val = LED[groupnum][lednum][i];
-        writeData( val );
-        msg += String( " " );
-        msg += String( val ); 
+        digitalWrite(latchPin, LOW);      
+        shiftOut(dataPin, clockPin, MSBFIRST,  Byte[i]);                   
+        digitalWrite(latchPin, HIGH); 
+    }    
+}
+
+
+void LedOFF(int row, int col)
+{
+
+    row--;
+    col--;
+
+    Byte[0] = (Byte[0] & ~LED[row][col][0]);
+    Byte[1] = (Byte[1] & ~LED[row][col][1]);
+    Byte[2] = (Byte[2] & ~LED[row][col][2]);
+    Byte[3] = (Byte[3] & ~LED[row][col][3]);
+    Byte[4] = (Byte[4] & ~LED[row][col][4]);
+
+    for(int i=0 ; i<5 ;i++)
+    {
+        digitalWrite(latchPin, LOW);      
+        shiftOut(dataPin, clockPin, MSBFIRST,  Byte[i]);                   
+        digitalWrite(latchPin, HIGH); 
+    }       
+
+    status_[row*NUM_RING+col] = false;
+}
+
+void ledON( size_t index )
+{
+    ring_ = index / NUM_RING  + 1;
+    led_ = ( index % NUM_LED_IN_RING ) + 1;
+    LedON( ring_, led_ );
+    last_active_[index] = millis();
+    status_[index] = true;
+}
+
+void ledOFF( size_t index )
+{
+    ring_ = index / NUM_RING  + 1;
+    led_ = ( index % NUM_LED_IN_RING ) + 1;
+    LedOFF( ring_, led_ );
+    status_[index] = false;
+    last_active_[index] = millis();
+}
+
+/* --------------------------------------------------------------------------*/
+/**
+ * @Synopsis  Led with given index.
+ *
+ * @Param index
+ */
+/* ----------------------------------------------------------------------------*/
+void update( size_t index )
+{
+    // Check if led is ON. If it is ON, switch it OFF.
+    if( status_[index] )
+    {
+        // Serial.print( "Index is on ");
+        // Serial.println( index );
+
+        if( (last_active_[ index ] - millis()) > 1000 )
+            ledOFF(index);
     }
-    Serial.println( msg );
-    delay( wait );
+    else
+        ledON( index );
+
 }
 
-void showLEDS( size_t wait = 100 )
-{
-    for (size_t i = 0; i < 6; i++) 
-        for (size_t ii = 0; ii < 6; ii++) 
-            turnON( i, ii, wait );
-}
-
-
-void testLEDSetup()
-{
-
-#if 1
-    // writeData( random(0,129) );
-    writeData( 0x20 );
-    delay( 500 );
-#else
-    showLEDS( 500 );
-#endif
-}
 
 void loop()
 { 
-    testLEDSetup();
-    delay(100);
+    for (size_t i = 0; i < NUM_LED_IN_RING * NUM_RING; i++) 
+        update( i );
+
+    delay( 200 );
 }
