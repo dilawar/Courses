a8dc157560944ca122c6cb6c142fa75d9288a0b2
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-04-15 12:10:15 +0530

    Problem 1 is solved. Need to solve problem 2.

--- TASHIP/InformationTheory2019/HW4/prob1.py
+++ TASHIP/InformationTheory2019/HW4/prob1.py
@@ -10,13 +10,32 @@ __email__            = "dilawars@ncbs.res.in"
 __status__           = "Development"
 
 import networkx as nx
+import math
+import numpy as np
+
+def entropy(pis):
+    e = 0.0
+    for p in pis:
+        if p > 0:
+            e -= p * math.log(p, 2)
+    return e
 
 def main():
     g = nx.DiGraph(nx.drawing.nx_agraph.read_dot('./ss.dot'))
     for s, t in g.edges():
         g[s][t]['prob'] = eval(g[s][t]['label'])
-    eAttrMat, label = nx.attr_matrix(g, 'prob')
+    eAttrMat, cols = nx.attr_matrix(g, 'prob')
+    print(cols)
+    u = np.ravel((eAttrMat ** 100)[0,:])
     print(eAttrMat)
+    print( '\nStationary distribution:' )
+    print(cols)
+    print(u)
+    rate = 0.0
+    for i, ui in enumerate(u):
+        pis = np.ravel(eAttrMat[i,:])
+        rate += ui * entropy(pis)
+    print( f'\nRate is: {rate:.3f} bits per symbol' )
 
 if __name__ == '__main__':
     main()
